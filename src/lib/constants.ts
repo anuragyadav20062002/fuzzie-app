f132a44c6a0289884cba4645b9333b24f4ca1fe6
Anurag Yadav <anuragyadav20062002@gmail.com>
2024-03-29 17:00:34 +0530

    added icons

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -82,3 +82,12 @@ export const clients = [...new Array(10)].map((client, index) => ({
       thumbnail: '/p3.png',
     },
   ]
+  export const menuOptions = [
+    { name: 'Dashboard', Component: Home, href: '/dashboard' },
+    { name: 'Workflows', Component: Workflows, href: '/workflows' },
+    { name: 'Settings', Component: Settings, href: '/settings' },
+    { name: 'Connections', Component: Category, href: '/connections' },
+    { name: 'Billing', Component: Payment, href: '/billing' },
+    { name: 'Templates', Component: Templates, href: '/templates' },
+    { name: 'Logs', Component: Logs, href: '/logs' },
+  ]
